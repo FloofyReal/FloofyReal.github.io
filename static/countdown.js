2f9aaea655a3caef2aa5423f6784c67079ab51e6
FloofyReal <adam.rafajdus@gmail.com>
2018-01-27 17:17:01 +0100

    Remove countdown from js files
    Signed-off-by: FloofyReal <adam.rafajdus@gmail.com>

--- static/countdown.js
+++ static/countdown.js
@@ -1,5 +1,4 @@
 // Set the date we're counting down to
-var countDownDate1 = new Date("Jan 14, 2018 23:59:59").getTime();
 var countDownDate2 = new Date("May 2, 2018 23:59:59").getTime();
 // var countDownDate3 = new Date("Dec 12, 2017 23:59:59").getTime();
 
@@ -10,21 +9,9 @@ var x = setInterval(function() {
     var now = new Date().getTime();
     
     // Find the distance between now an the count down date
-    var distance1 = countDownDate1 - now;
     var distance2 = countDownDate2 - now;
     
     // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance1 / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance1 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance1 % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance1 % (1000 * 60)) / 1000);
-
-    // Output the result in an element with id="demo"
-    document.getElementById("dp2").innerHTML = days + " days " 
-    + hours + " hours " + minutes + " minutes "
-    + seconds + " seconds ";
-    
-
     var days = Math.floor(distance2 / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance2 % (1000 * 60 * 60)) / (1000 * 60));
@@ -36,11 +23,6 @@ var x = setInterval(function() {
     + seconds + " seconds ";
     
     // If the count down is over, write some text 
-    if (distance1 < 0) {
-        clearInterval(x);
-        document.getElementById("dp2").innerHTML = "U R FUCKED BOI";
-    }
-
     if (distance2 < 0) {
         clearInterval(x);
         document.getElementById("dp3").innerHTML = "U R FUCKED BOI";
