48cc9e06ac49204d011443efccbbec0bd5e70265
FloofyReal <adam.rafajdus@gmail.com>
2018-05-09 00:23:28 +0200

    Change date to approx. of day of presentation
    Signed-off-by: FloofyReal <adam.rafajdus@gmail.com>

--- static/countdown.js
+++ static/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate2 = new Date("May 2, 2018 23:59:59").getTime();
+var countDownDate2 = new Date("June 5, 2018 23:59:59").getTime();
 // var countDownDate3 = new Date("Dec 12, 2017 23:59:59").getTime();
 
 // Update the count down every 1 second
